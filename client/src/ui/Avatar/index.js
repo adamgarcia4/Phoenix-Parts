4de8dd911fc670e4535f31736326897350e325b0
Adam Garcia <adamgarcia1994@ucla.edu>
2018-08-23 23:47:14 +0300

    Refactored Avatar and Avatar Group
    Now uses Flexbox and styled-components instead of css files.

--- client/src/ui/Avatar/index.js
+++ client/src/ui/Avatar/index.js
@@ -4,11 +4,64 @@ import Avatar from '@material-ui/core/Avatar'
 import Button from '@material-ui/core/Button'
 import deepOrange from '@material-ui/core/colors/deepOrange'
 import deepPurple from '@material-ui/core/colors/deepPurple'
-import cx from 'classnames'
-import './Styles.css'
+import styled from 'styled-components'
 
 function LetterAvatars(props) {
-  const { user, isImage = true, overlap } = props
+  const { user, isImage = true, overlap, border } = props
+
+  // .avatar {
+  //   display: inline-block;
+  //   text-align: center;
+  //   line-height: 50px;
+  //   width: 50px;
+  //   height: 50px;
+  //   background-color: aqua;
+  //   font-size: 24px;
+  //   border-radius: 50%;
+  //   border: 3px solid #fff;
+  //   box-sizing: border-box;
+  //   vertical-align: top;
+  //   overflow: hidden;
+  //   position: relative;
+  //   margin-right: 0px;
+  //   margin-left: -15px;
+  //   transition: margin-left .15s ease-in;
+  // }
+
+  // .avatar:first-child {
+  //   margin-left: 0;
+  // }
+
+  // TODO: Add sm md lg properties
+  // TODO: Add bg and color overrides
+  const BaseAvatar = styled.div`
+    /* General Box Sizing */
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: #bdbdbd;
+    color: white;
+    margin: 10px;
+    border: ${props => (props.border ? '2px solid #fff' : '')};
+    transform: ${props => (props.overlap ? 'translateX(-40px)' : '')};
+
+    /* Layout Formatting */
+    display: inline-flex;
+    flex-shrink: 0;
+    align-items: center;
+    justify-content: center;
+    position: relative; /* Allows for relative placement */
+
+    /* Other */
+    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+    font-size: 1.25rem;
+    overflow: hidden;
+
+    img {
+      height: 40px;
+      width: auto;
+    }
+  `
 
   const getInitials = (first, last) => {
     return `${first.charAt(0)}${last.charAt(0)}`
@@ -34,12 +87,16 @@ function LetterAvatars(props) {
   const getAvatar = isImage => {
     if (isImage) {
       return (
-        <div className={'avatar'}>
+        <BaseAvatar overlap={overlap} border={border}>
           <img src="https://media.licdn.com/dms/image/C5103AQHoTTrJ1xgdvA/profile-displayphoto-shrink_200_200/0?e=1539820800&v=beta&t=KUOCOe6XZd31WwgpmLMrccWmgYMiFT8PNi0RQPftpIs" />
-        </div>
+        </BaseAvatar>
       )
     } else {
-      return <div className={cx('avatar')}>AG</div>
+      return (
+        <BaseAvatar overlap={overlap} border={border}>
+          AG
+        </BaseAvatar>
+      )
     }
   }
 
--- client/src/ui/AvatarGroup/index.js
+++ client/src/ui/AvatarGroup/index.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react'
-import './Styles.css'
+import styled from 'styled-components'
 
+// TODO: Add onHover
+// .avatarGroup:hover .avatar {
+//   margin-left: 0px;
+// }
+
+const BaseAvatarGroup = styled.div`
+  display: flex;
+  justify-content: center;
+`
 class AvatarGroup extends Component {
   getAvatarGroup() {
     const avatars = this.props.children
     // return avatars
 
     return React.Children.map(avatars, (avatar, i) => {
-      if (i === 0) return avatar
+      const isOverlap = i !== 0
 
-      return React.cloneElement(avatar, { overlap: true })
+      return React.cloneElement(avatar, { overlap: isOverlap, border: true })
     })
   }
 
   render() {
-    return <div className={'avatarGroup'}>{this.getAvatarGroup()}</div>
+    return <BaseAvatarGroup>{this.getAvatarGroup()}</BaseAvatarGroup>
   }
 }
 
