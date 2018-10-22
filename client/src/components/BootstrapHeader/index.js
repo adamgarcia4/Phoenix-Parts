4a84bc72b6b967a9a4fd4aaa9797ce616298a7d4
Adam Garcia <adamgarcia1994@ucla.edu>
2018-10-22 09:58:47 +0300

    Header navbar now working

--- client/src/components/BootstrapHeader/index.js
+++ client/src/components/BootstrapHeader/index.js
@@ -1,29 +1,32 @@
 import React, { Component } from 'react'
 
+import { NavLink } from 'react-router-dom'
+import HeaderNavLink from '../HeaderNavLink'
 const TOGGLE_HANDLE = 'app-navbar'
+
 
 {/* 
 
 
 
-<div class="collapse navbar-collapse" id="navbarColor01">
-  <ul class="navbar-nav mr-auto">
-    <li class="nav-item active">
-      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+<div className="collapse navbar-collapse" id="navbarColor01">
+  <ul className="navbar-nav mr-auto">
+    <li className="nav-item active">
+      <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
     </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Features</a>
+    <li className="nav-item">
+      <a className="nav-link" href="#">Features</a>
     </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Pricing</a>
+    <li className="nav-item">
+      <a className="nav-link" href="#">Pricing</a>
     </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">About</a>
+    <li className="nav-item">
+      <a className="nav-link" href="#">About</a>
     </li>
   </ul>
-  <form class="form-inline">
-    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+  <form className="form-inline">
+    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button className="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
   </form>
 </div>
 */}
@@ -34,55 +37,60 @@ class BootstrapHeader extends Component {
   getHeader() {
 
     const getBrand = () => {
-      return <a class="navbar-brand" href="#">Expand at md</a>
+      return <a className="navbar-brand" href="#">Expand at md</a>
     }
 
     const getHamburgerToggle = () => {
       return (
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target={`#${TOGGLE_HANDLE}`} aria-controls={TOGGLE_HANDLE} aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target={`#${TOGGLE_HANDLE}`} aria-controls={TOGGLE_HANDLE} aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
       )
     }
 
     const getItemLinks = () => {
 
+      
+
       return (
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <ul className="navbar-nav mr-auto">
+
+          <HeaderNavLink to='' title='Home' exact/>
+          <HeaderNavLink to='dashboard' title='Dashboard'/>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#">Disabled</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-            <div class="dropdown-menu" aria-labelledby="dropdown04">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="https://example.com" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+            <div className="dropdown-menu" aria-labelledby="dropdown04">
+              <a className="dropdown-item" href="#">Action</a>
+              <a className="dropdown-item" href="#">Another action</a>
+              <a className="dropdown-item" href="#">Something else here</a>
             </div>
           </li>
         </ul>
       )
     }
 
+    const getRightForm = () => {
+
+      return (
+        <form className="form-inline my-2 my-md-0">
+          <input className="form-control" type="text" placeholder="Search" />>
+      </form>
+      )
+    }
+
     return (
       <div>
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
           {getBrand()}
           {getHamburgerToggle()}
 
-
-          <div class="collapse navbar-collapse" id={TOGGLE_HANDLE}>
+          <div className="collapse navbar-collapse" id={TOGGLE_HANDLE}>
             {getItemLinks()}
-            
-            <form class="form-inline my-2 my-md-0">
-              <input class="form-control" type="text" placeholder="Search" />>
-            </form>
+
+            {getRightForm()}
           </div>
         </nav>
         {this.props.children}
