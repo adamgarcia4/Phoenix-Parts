91ebcea6ddc1ff45f60eed4df2a7a95413913042
Adam Garcia <adamgarcia1994@ucla.edu>
2018-11-07 10:17:18 +0200

    Started pulling hardcoded inside of card into props

--- client/src/components/PartsDashboard/index.js
+++ client/src/components/PartsDashboard/index.js
@@ -4,33 +4,39 @@ import Well from '../../ui/Well'
 import './Style.css'
 class PartsDashboard extends Component {
   render() {
+    const getCards = () => {
+      const getCard = index => {
+        return (
+          <div className="parts-item" key={index}>
+            <Card>
+              <Card.CardPicture />
+              <Card.CardContent>
+                <h2>Drive Shaft</h2>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis eos animi molestiae, suscipit
+                distinctio aliquid sequi necessitatibus quam ut quis voluptate quas ipsa in iure, sapiente facilis
+                beatae incidunt.
+              </Card.CardContent>
+              <Card.CardContentActions />
+            </Card>
+          </div>
+        )
+      }
+      const test = Array.apply(null, { length: 10 })
+        .map(Number.call, Number)
+        .map(num => {
+          return getCard(num)
+        })
+
+      console.log('test:', test)
+
+      return test
+    }
+
     return (
       <Fragment>
         <h1 className="parts-title">Parts Container</h1>
         <Well className="parts-well">
-          <div className="parts-container">
-            <div className="parts-item">
-              <Card />
-            </div>
-            <div className="parts-item">
-              <Card />
-            </div>
-            <div className="parts-item">
-              <Card />
-            </div>
-            <div className="parts-item">
-              <Card />
-            </div>
-            <div className="parts-item">
-              <Card />
-            </div>
-            <div className="parts-item">
-              <Card />
-            </div>
-            <div className="parts-item">
-              <Card />
-            </div>
-          </div>
+          <div className="parts-container">{getCards()}</div>
         </Well>
       </Fragment>
     )
--- client/src/ui/Card/index.js
+++ client/src/ui/Card/index.js
@@ -6,26 +6,35 @@ const CardPicture = props => {
   return <div className="card-picture" title="Test" />
 }
 
+const CardContent = props => {
+  return <div className="card-content">{props.children}</div>
+}
+
+const CardContentActions = props => {
+  return (
+    <div className="card-content-actions">
+      <button type="button" className="btn btn-primary">
+        Go to part
+      </button>
+      <button type="button" className="btn btn-secondary">
+        Edit
+      </button>
+    </div>
+  )
+}
+
 class Card extends Component {
   static CardPicture = CardPicture
+  static CardContent = CardContent
+  static CardContentActions = CardContentActions
   render() {
     return (
       <Paper className="card-container">
-        <div className="card-picture" title="Test" />
-        <div className="card-content">
-          <h2>Drive Shaft</h2>
+        {this.props.children}
+        {/* <h2>Drive Shaft</h2>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis eos animi molestiae, suscipit
           distinctio aliquid sequi necessitatibus quam ut quis voluptate quas ipsa in iure, sapiente facilis beatae
-          incidunt.
-        </div>
-        <div className="card-content-actions">
-          <button type="button" className="btn btn-primary">
-            Go to part
-          </button>
-          <button type="button" className="btn btn-secondary">
-            Edit
-          </button>
-        </div>
+          incidunt. */}
       </Paper>
     )
   }
